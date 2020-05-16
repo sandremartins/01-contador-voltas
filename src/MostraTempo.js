20341fb9f02acf6a89da8c793a35ac4825d8ca6c
Alessandre Martins <sandremartins@gmail.com>
2020-05-16 12:20:32 -0400

    Inicial Contador de Voltas em React

---
+++ src/MostraTempo.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const MostraTempo = (props) => {
+    const tempo = props.tempo
+    const minutos = Math.round(tempo / 60)
+    const segundos = tempo % 60
+    const minutosStr = minutos < 10 ? '0' + minutos : minutos
+    const segundosStr = segundos < 10 ? '0' + segundos : segundos
+    return (
+      <p>
+        <span>{`${minutosStr}:${segundosStr}`}<br /></span>
+        Tempo médio por volta
+      </p>
+    )
+  }
+export default MostraTempo
---
+++ src/MostraVoltas.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MostraVoltas = (props) => {
+    return (
+        <p>
+            <span className='voltas'>{props.voltas}<br /></span>
+        Voltas
+        </p>
+    )
+}
+export default MostraVoltas
